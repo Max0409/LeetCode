47c6f5ad25433470565df0060e026cd49984637f
chenyue <1796004750@qq.com>
2019-12-03 20:50:20 +0800

    41 缺失的第一个正数

--- 41.cpp
+++ 41.cpp
@@ -7,20 +7,29 @@ using namespace std;
 
 
 int firstMissingPositive(vector<int>& nums) {
+
     for(int i=0;i<nums.size();i++){
-        if(nums[i]!=i+1){
-            swap(nums[i],nums[i+1]);
-            i--;
+        while(nums[i]>0&&nums[i]<=nums.size()&&nums[i]!=(i+1)){
+            int loc=nums[i]-1;
+            if(nums[loc]==nums[i])
+                break;
+
+            swap(nums[loc],nums[i]);
 
         }
+
     }
 
 
     for(int i=0;i<nums.size();i++){
+
         if(nums[i]!=i+1){
             return i+1;
 
         }
     }
+
+    return nums.size()+1;
+
 
 }
